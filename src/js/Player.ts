e9355175ec562c695684ee55afe4f86cc892d09a
pomerama <marpom@gmail.com>
2023-10-29 20:30:00 +0100

    minimal version with player animated and jumping

--- src/js/Player.ts
+++ src/js/Player.ts
@@ -1,34 +1,21 @@
 export class Player {
-    width: number;
-    height: number;
-    positionX: number;
-    positionY: number;
-    playerElm: HTMLElement;
+    playerElement: HTMLElement;
+    isJumping: boolean;
 
     constructor() {
-
-        this.width = 20;
-        this.height = 10;
-        this.positionX = 50 - (this.width / 2);
-        this.positionY = 0;
-
-
-        this.playerElm = document.querySelector(".player")!;
-        this.playerElm.style.left = this.positionX + "vw";
-        this.playerElm.style.bottom = this.positionY + "vh";
-    }
-    moveLeft() {
-        this.positionX--;
-        this.playerElm.style.left = this.positionX + "vw";
-    }
-    moveRight() {
-        this.positionX++;
-        this.playerElm.style.left = this.positionX + "vw";
+        this.isJumping = false;
+        this.playerElement = document.querySelector(".player")!;
     }
     jump() {
-        console.log("JUMP")
-        console.log(this.positionY)
-        this.positionY++;
-        this.playerElm.style.top = this.positionY + "vw"
+        if (this.isJumping) return;
+        if (this.playerElement.classList.contains('in-jump')) return;
+
+        this.isJumping = true;
+        this.playerElement.classList.add('in-jump')
+        setTimeout(() => {
+            this.playerElement.classList.remove('in-jump')
+        }, 500);
+
+        this.isJumping = false;
     }
 }
